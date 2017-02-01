eaea375e42886a51d9156947c27f4bf18b6e612b
Gguyot <georgesguyot59@gmail.com>
2017-02-01 18:17:38 +0100

    - Calcul de la position de la balle à partir de l'origine (tireur) et de la distance que celle-ci va parcourir - Ajout de la classe du service ( reste l'utilisation de celle-cii) - Affichage de la position de la balle sur la map - Ajout du suivi de l'utilisateur lors de ses déplacements pour rejoindre le point de chute de la balle

--- app/src/main/java/com/example/georges/projectgolf/MapGoogleGolf.java
+++ app/src/main/java/com/example/georges/projectgolf/MapGoogleGolf.java
@@ -19,6 +19,7 @@ import com.google.android.gms.maps.GoogleMap;
 import com.google.android.gms.maps.MapFragment;
 import com.google.android.gms.maps.OnMapReadyCallback;
 import com.google.android.gms.maps.SupportMapFragment;
+import com.google.android.gms.maps.model.BitmapDescriptorFactory;
 import com.google.android.gms.maps.model.CameraPosition;
 import com.google.android.gms.maps.model.LatLng;
 import com.google.android.gms.maps.model.Marker;
@@ -34,12 +35,12 @@ public class MapGoogleGolf extends FragmentActivity implements OnMapReadyCallbac
 
 
 
-    protected GoogleApiClient mGoogleApiClient;
+
     //Location locationTemp;
     double lat = 0, lng = 0;
     String provider;
     LocationManager locationManager;
-
+    GoogleMap mMap;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -57,6 +58,9 @@ public class MapGoogleGolf extends FragmentActivity implements OnMapReadyCallbac
        /* LatLng sydney = new LatLng(-34, 151);
         map.addMarker(new MarkerOptions().position(sydney).title("Marqueur sur Sydney"));
         map.moveCamera(CameraUpdateFactory.newLatLng(sydney));*/
+
+
+        mMap=map;
         if (ActivityCompat.checkSelfPermission(this, Manifest.permission.ACCESS_FINE_LOCATION) != PackageManager.PERMISSION_GRANTED && ActivityCompat.checkSelfPermission(this, Manifest.permission.ACCESS_COARSE_LOCATION) != PackageManager.PERMISSION_GRANTED) {
             // TODO: Consider calling
             //    ActivityCompat#requestPermissions
@@ -83,44 +87,82 @@ public class MapGoogleGolf extends FragmentActivity implements OnMapReadyCallbac
         // Getting Current Location
         Location locationTemp = locationManager.getLastKnownLocation(provider);
 
-
         locationManager.requestLocationUpdates(LocationManager.GPS_PROVIDER, 0, 0, this);
+
+
+        Log.e("LOCALISATION", "POS GOOGLE LATITUDE : " + locationTemp.getLatitude() + "POS THOERIQUE : " + 50.166689 + " ||     POS GOOGLE LONGETITUDE : " + locationTemp.getLongitude() + "  POS THEORIQUE : " + 3.159122000000025 + "    ||||||||      " + locationTemp.getAccuracy());
+
         if (locationTemp != null && locationTemp.getAccuracy() >= 100) {
-            Log.e("LOCALISATION", "POS GOOGLE LATITUDE : " + locationTemp.getLatitude() + "POS THOERIQUE : " + 50.166689 + " ||     POS GOOGLE LONGETITUDE : " + locationTemp.getLongitude() + "  POS THEORIQUE : " + 3.159122000000025 + "    ||||||||      " + locationTemp.getAccuracy());
             LatLng loc = new LatLng(locationTemp.getLatitude(), locationTemp.getLongitude());
-            map.animateCamera(CameraUpdateFactory.newLatLngZoom(loc, 16.0f));
+            CameraPosition camera=new CameraPosition.Builder()
+                    .target(loc)
+                    .zoom(17)
+                    .build();
+            map.animateCamera(CameraUpdateFactory.newCameraPosition(camera));
             map.addMarker(new MarkerOptions()
                     .position(new LatLng(loc.latitude, loc.longitude))
                     .title("Vous êtes ici"));
-
         }
 
     }
-
-
     @Override
     public void onLocationChanged(Location location) {
-
 
     // Getting Current Location
         if(location.getAccuracy()<100)
         {
             LatLng loc = new LatLng(location.getLatitude(), location.getLongitude());
-           /* CameraPosition camera=new CameraPosition.Builder()
+            CameraPosition camera=new CameraPosition.Builder()
                     .target(loc)
-                    .zoom(17)
+                    .zoom(15)
                     .build();
             mMap.animateCamera(CameraUpdateFactory.newCameraPosition(camera));
-
-            mMap.addMarker(new MarkerOptions()
-                    .position(new LatLng(loc.latitude, loc.longitude))
-                    .title("Vous êtes ici"));*/
-
+            ballPosition(location);
         }
     Log.e("PositionChange",location.getLatitude()+"     "+location.getLongitude()+"       "+location.getAccuracy());
     }
 
 
+    /*Fonction permettant d'afficher la position de la balle  sur la carte
+    A partir des données :
+        - position du tireur
+        - distance de parcours
+    MODIFICATION à ajouter
+        - Choisir la distance (par défaut 45°)
+        - Utiliser la distance récupéré (actuellement la distance est en dur)
+    SOURCE
+        - Formule
+            # http://www.movable-type.co.uk/scripts/latlong.html
+        - Site permettant de vérifier les test
+            # http://www.geomidpoint.com/destination/
+    */
+
+    public void ballPosition(Location Location)
+    {
+        double R = 6378.1;  // Rayon de la terre
+        double brng = Math.toRadians(45); // Direction
+        double d = 120* 0.001; // Distance en m
+
+        double lat = Math.toRadians(50.16848049999); // Position actuelle latitude radian
+        double lon = Math.toRadians(3.159599299999); // Position actuelle Longétitude radian
+
+        double lat2=Math.asin( Math.sin(lat)*Math.cos(d/R) +
+                Math.cos(lat)*Math.sin(d/R)*Math.cos(brng)); // Calcul de la latitude de la balle
+
+        double lon2=lon + Math.atan2(Math.sin(brng)*Math.sin(d/R)*Math.cos(lat),
+                Math.cos(d/R)-Math.sin(lat)*Math.sin(lat2));// Calcul de la longétitude de la balle
+
+        // Résultat
+        double finalLat=Math.toDegrees(lat2);
+        double finalLon=Math.toDegrees(lon2);
+        // Ajout du marqueur à la position
+        LatLng loc = new LatLng(finalLat, finalLon);
+        mMap.addMarker(new MarkerOptions()
+                .position(new LatLng(loc.latitude, loc.longitude))
+                .icon(BitmapDescriptorFactory.defaultMarker(BitmapDescriptorFactory.HUE_AZURE))
+                .title("Vous êtes ici"));
+        Log.e("VALEUR","Latitude   : "+finalLat+"               longitude    :  "+finalLon);
+    }
 
     @Override
     public void onStatusChanged(String provider, int status, Bundle extras) {
---
+++ app/src/main/java/com/example/georges/projectgolf/ServiceGPS.java
@@ -0,0 +1,84 @@
+package com.example.georges.projectgolf;
+
+import android.Manifest;
+import android.app.Service;
+import android.content.Intent;
+import android.content.pm.PackageManager;
+import android.location.Location;
+import android.location.LocationListener;
+import android.location.LocationManager;
+import android.os.Bundle;
+import android.os.IBinder;
+import android.support.v4.app.ActivityCompat;
+import android.util.Log;
+
+/**
+ * Created by Georges on 01/02/2017.
+ */
+// Lancement de la localisation
+public class ServiceGPS extends Service implements LocationListener {
+    LocationManager mLocationManager;
+
+    public IBinder onBind(Intent intent) {
+        // This won't be a bound service, so simply return null
+        Log.v("WEAVER_", "Service_OnBind...");
+        return null;
+    }
+
+    @Override
+    public void onCreate() {
+        // This will be called when your Service is created for the first time
+        // Just do any operations you need in this method.
+        Log.v("WEAVER_", "Service_OnCreate...");
+
+        mLocationManager = (LocationManager) getSystemService(LOCATION_SERVICE);
+
+        if (ActivityCompat.checkSelfPermission(this, Manifest.permission.ACCESS_FINE_LOCATION) != PackageManager.PERMISSION_GRANTED && ActivityCompat.checkSelfPermission(this, Manifest.permission.ACCESS_COARSE_LOCATION) != PackageManager.PERMISSION_GRANTED) {
+            // TODO: Consider calling
+            //    ActivityCompat#requestPermissions
+            // here to request the missing permissions, and then overriding
+            //   public void onRequestPermissionsResult(int requestCode, String[] permissions,
+            //                                          int[] grantResults)
+            // to handle the case where the user grants the permission. See the documentation
+            // for ActivityCompat#requestPermissions for more details.
+            return;
+        }
+        mLocationManager.requestLocationUpdates(LocationManager.GPS_PROVIDER, 1, 1, this);
+    }
+
+    @Override
+    public int onStartCommand(Intent intent, int flags, int startId) {
+        return super.onStartCommand(intent, flags, startId);
+    }
+
+    @Override
+    public void onDestroy() {
+        super.onDestroy();
+        Log.v("WEAVER_", "Service_OnDestroy");
+        if (ActivityCompat.checkSelfPermission(this, Manifest.permission.ACCESS_FINE_LOCATION) != PackageManager.PERMISSION_GRANTED && ActivityCompat.checkSelfPermission(this, Manifest.permission.ACCESS_COARSE_LOCATION) != PackageManager.PERMISSION_GRANTED) {
+
+            return;
+        }
+        mLocationManager.removeUpdates(this);
+        mLocationManager = null;
+    }
+    @Override
+    public void onLocationChanged(Location location) {
+        Log.v("WEAVER_","Service_Location Change");
+    }
+
+    @Override
+    public void onStatusChanged(String provider, int status, Bundle extras) {
+
+    }
+
+    @Override
+    public void onProviderEnabled(String provider) {
+
+    }
+
+    @Override
+    public void onProviderDisabled(String provider) {
+
+    }
+}
